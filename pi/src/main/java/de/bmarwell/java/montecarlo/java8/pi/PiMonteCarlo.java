11394dd85439317fb91cb19d2b9d52f46db0f1b0
Benjamin Marwell <bmarwell@gmail.com>
2016-05-12 21:22:49 +0200

    Create submodule pi.

---
+++ pi/src/main/java/de/bmarwell/java/montecarlo/java8/pi/PiMonteCarlo.java
@@ -0,0 +1,63 @@
+/**
+ *
+ */
+package de.bmarwell.java.montecarlo.java8.pi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+import java.util.stream.LongStream;
+
+/**
+ * @author bmarwell
+ *
+ */
+public class PiMonteCarlo {
+
+  /**
+   * Logger…
+   */
+  private static final Logger LOG = LoggerFactory.getLogger(PiMonteCarlo.class);
+
+  /**
+   * Start pi calculation.
+   *
+   * @param args
+   *          the first arg is how many tries we use.
+   */
+  public static void main(String[] args) {
+    if (args.length == 0) {
+      LOG.error("Parameter: number of tries missing");
+
+      throw new IllegalArgumentException("Paramter: number of tries missing.");
+    }
+
+    long tries = Long.parseLong(args[0]);
+    Random random = new Random();
+    random.setSeed(System.currentTimeMillis());
+
+    /*
+     * The parallel is important, as we want to use all cores. In a professional environment, we
+     * would inject an ExecutorService instead.
+     *
+     * Also, we need to declare the num, but we just don't use it.
+     *
+     * Then, we just count the number of hits.
+     */
+    long hits = LongStream.range(0, tries)
+        .parallel()
+        .filter(num -> PiMonteCarlo.isHit(random))
+        .count();
+
+    LOG.info("Pi is about [{}].", hits * 4.0 / tries);
+  }
+
+  public static boolean isHit(Random random) {
+    double x = random.nextDouble();
+    double y = random.nextDouble();
+
+    return Math.hypot(x, y) <= 1;
+  }
+
+}
